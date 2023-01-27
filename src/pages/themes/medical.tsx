c079f1feddcb2e9bd2355de4bc052fa70c79e6e0
Prakash Poudel <prakash@truemark.com.np>
2023-01-27 16:36:22 +0545

    Themes marketplace

---
+++ src/pages/themes/medical.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main>
+      Polo Themes
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        WOSA fashion theme
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <>
+<title>Home | Polo Themes</title> 
+<meta name="description" content="Polothemes sells e-commerce themes (figma, tailwindcss, nextjs), currently nextjs themes only support shopify"></meta>
+</>
