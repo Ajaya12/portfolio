f5c1c48d32497f7fe454df07ec411dc985f3e77d
Ajaya Thakur <ajayathakur383@gmail.com>
2023-02-24 14:20:59 +0545

    Initial commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,53 +2,34 @@ import * as React from "react";
 import { HeadFC, Link, PageProps } from "gatsby";
 
 import copyright from "../images/footer/copyright.svg";
+import allbrainyLogo from "../images/svg/allbrainy-logo.svg";
 
 const Footer: React.FC<PageProps> = () => (
-  <div className="bg-footer-color text-center p-6 lg:p-24 lg:pb-6 lg:pt-44 text-white relative">
-    <div className="flex justify-center mb-10 -mt-48">
-      <div className="p-10 border bg-white rounded sm:w-1/2">
-        <h2 className="text-3xl text-primary-text-color font-medium mb-4">
-          Subscribe to our newsletter!
-        </h2>
-        <p className="text-secondary-text-color mb-10">
-          Sign up for our newsletter and stay in the loop on all the latest
-          updates and exclusive content.
-        </p>
-        <label className="relative flex justify-center">
-          <input
-            className="placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-4 pl-4 pr-8 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm 2xl:w-2/4 xl:w-3/4 w-full"
-            placeholder="Your Email Address"
-            type="text"
-            name="email"
-          />
-          <button className="btn-primary absolute right-[26%]">
-            Subscribe
-          </button>
-        </label>
-      </div>
-    </div>
+  <div className="bg-footer-color text-center p-12 text-text-color-600 relative">
     <div>
-      <h2 className=" mb-6 text-xl font-medium">POLO THEMES</h2>
-      <div className="md:flex md:gap-6 justify-center mb-12">
+      <div className="flex justify-center items-center gap-1 mb-2.5">
+        <img src={allbrainyLogo} alt="all brainy logo" />
+        <h2 className="text-2xl font-semibold text-text-color-500">
+          All<span className="text-blue-500">Brainy</span>
+        </h2>
+      </div>
+      <p className="mb-14">8 The Green STE R, Dover, Delaware, USA, 19901</p>
+      <div className="md:flex md:gap-6 justify-center mb-12 text-text-color-900">
         <div className="flex justify-center gap-6 mb-6 md:mb-0">
-          <p>Terms and Conditions</p>
-          <p>Support</p>
+          <p>Home</p>
+          <p>Products</p>
+          <p>Resources</p>
         </div>
         <div className="sm:flex sm:gap-6 justify-center">
           <div className="flex gap-6 justify-center mb-6 sm:mb-0">
-            <p>Privacy Policy</p>
-            <p>Licensing</p>
-          </div>
-          <div className="flex justify-center gap-6">
-            <p>Help Center</p>
-            <p>About</p>
+            <p>About us</p>
+            <p>Solutions</p>
+            <p>Pricing</p>
           </div>
         </div>
       </div>
-      <span className="flex justify-center item-center gap-2">
-        <img src={copyright} />
-        <p className="text-white">2023 copyright All right Reserved</p>
-      </span>
+
+      <p className="text-xs">© Copyright 2023 - Allbrainy</p>
     </div>
   </div>
 );
--- src/components/homepage/herosection.tsx
+++ src/components/homepage/herosection.tsx
@@ -1,42 +1,33 @@
 import * as React from "react";
 import { HeadFC, Link, PageProps } from "gatsby";
 
-import image1 from "../../images/home/image01.jpg";
-import image2 from "../../images/home/image02.jpg";
-import image3 from "../../images/home/image03.jpg";
-import image4 from "../../images/home/image04.jpg";
-import image5 from "../../images/home/image05.jpg";
+import allbrainyLogo from "../../images/svg/allbrainy-logo.svg";
+import truemarkLogo from "../../images/svg/truemark-logo.svg";
 
 const HeroSection: React.FC<PageProps> = () => (
   <div>
     <div className="text-center mb-12">
-      <h1 className="text-2xl lg:text-4xl md:text-3xl font-medium text-black mb-6">
-        Discover the <span className="text-blue-500">Best Themes</span> for Your
-        Website
+      <div className="flex mb-2.5 justify-center">
+        <img src={allbrainyLogo} alt="All brainy logo" />
+      </div>
+      <h1 className="text-3xl lg:text-5xl md:text-4xl font-bold text-blue-500 mb-2.5">
+        AllBrainy
       </h1>
+      <h2 className="text-2xl md:text-3xl font-bold text-text-color-900 mb-2.5">
+        Product Development Lab
+      </h2>
+      <span className="flex justify-center items-center gap-2 mb-8">
+        <p className="text-2xl font-bold text-truemark-primary">by</p>
+        <img src={truemarkLogo} alt="" />
+      </span>
       <div className=" flex justify-center">
-        <p className="text-primary-text-color w-10/12 md:w-7/12">
-          Find the perfect theme for your online presence with our curated
-          collection of the most stunning and functional themes available.
+        <p className="text-primary-text-color mb-8 w-10/12 md:w-7/12">
+          Our mission is to provide intelligent solutions that help businesses
+          work smarter, not harder. With Allbrainy, you can streamline your
+          processes and achieve better results with less effort.
         </p>
       </div>
-    </div>
-    <div className=" align-bottom relative flex justify-center">
-      <div className="absolute bottom-0 w-1/4 z-20 hero-image-left">
-        <img src={image4} className="z-10 " />
-      </div>
-      <div className="absolute bottom-0 left-0 w-1/4">
-        <img src={image5} className="z-10" />
-      </div>
-      <div className="w-1/3 z-30">
-        <img src={image1} />
-      </div>
-      <div className="absolute bottom-0 hero-image-right w-1/4 z-20">
-        <img src={image2} />
-      </div>
-      <div className="absolute right-0 bottom-0 w-1/4 z-10">
-        <img src={image3} />
-      </div>
+      <button className="btn-primary">Our Products</button>
     </div>
   </div>
 );
